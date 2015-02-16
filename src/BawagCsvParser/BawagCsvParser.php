8545c39737bde470fc44f3b3655271a19606dd1a
PeterTheOne <petertheone@gmail.com>
2015-02-16 18:46:04 +0100

    fix DateTime namespace

--- src/BawagCsvParser/BawagCsvParser.php
+++ src/BawagCsvParser/BawagCsvParser.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace BawagCsvParser;
+use DateTime;
 
 /**
  * Class BawagPskParser
@@ -24,7 +25,7 @@ class BawagCsvParser {
             //$line = str_replace(["\n", "\t", "\r"],['', '', ''], $line);
             $csvLine = str_getcsv($line, ';');
 
-            $date = new \DateTime($csvLine[2]);
+            $date = new DateTime($csvLine[2]);
 
             // remove leading plus sign
             $csvLine[4] = ltrim($csvLine[4], '+$');
